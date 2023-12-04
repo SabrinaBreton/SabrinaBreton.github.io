8546061e59462a43248d6c751415983c57071137
SabrinaBreton <2352658@carrefour.cegepvicto.ca>
2023-12-04 09:57:43 -0500

    Version finale

--- js/main.js
+++ js/main.js
@@ -45,7 +45,6 @@ function handleTurn() {
     win = getWinner();
     render();
 };
-
 function init() {
     board = [
     '', '', '',
@@ -53,14 +52,49 @@ function init() {
     '', '', ''
     ];
     render();
+    //Appel fonction
+    resetBackground();
 };
 
 function render() {
     board.forEach(function(mark, index) {
     //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
+    //Pour chaque case, on enleve la classe checked
+    squares[index].classList.remove("checked");
+    //Si la case est n'est pas vide
+    if(mark !== "")
+    {
+        //On ajoute la classe checked
+        squares[index].classList.add("checked")
+    }
     });
-    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `Partie nulle!` : win ? `${win} a gagné le jeu!` : `C'est le tour de ${turn}!`;
+    //Appel fonction
+    changerBackground();
     };
-
 init();
+//Fonction qui change le fond de la page
+function changerBackground()
+{
+    //Quand win = tie/egalite
+    if(win === "T")
+    {
+        //Change couleur du document
+      document.body.style.backgroundColor = "red";
+    }
+}
+//Fonction qui fait un retour au depart
+function resetBackground()
+{
+    //Change la couleur pour celle du depart
+    document.body.style.backgroundColor = "";
+}
+//Ajouter un eventlistener pour que lorque le bouton est appuyer, le init est appeler
+document.getElementById("reset-button").addEventListener("click", function()
+{
+    //Init contient resetBackground
+    init();
+})
+//*Modifications: Dans render, init et les fonctions "changerBackground"
+//"resetBackground"
